533bc77dba30fce33d617b4614c1d49a3bea3986
Fisgche <gustavooliveirafischer@hotmail.com>
2026-01-13 21:10:30 -0300

    refatora a string de conexao hardcoded

--- myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -1,17 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using myfinance_web_dotnet_domain.Entities;
 
 namespace myfinance_web_dotnet_infra;
 
 public class MyFinanceDbContext : DbContext
 {
+    private readonly IConfiguration _configuration;
     public DbSet<PlanoConta> PlanoConta {get; set;}
 
     public DbSet<Transacao> Transacao {get; set;}
+
+    public MyFinanceDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=DESKTOP-8M751P2\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=False;");
-        //optionsBuilder.UseSqlServer(@"Server=meuservidorsqlserver.database.windows.net;Database=myfinance;User Id=user;Password=*********;Connect Timeout=60;");
+        //optionsBuilder.UseSqlServer(@"Server=meuservidorsqlserver.database.windows.net;Database=myfinance;User Id=user;Password=*********;Connect Timeout=60;");  
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("Database"));
+        
         base.OnConfiguring(optionsBuilder);
     }
 }
