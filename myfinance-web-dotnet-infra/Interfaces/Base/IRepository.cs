23efc4255b577516214d34784eed499304bfa718
Fisgche <gustavooliveirafischer@hotmail.com>
2026-01-13 21:34:37 -0300

    Implementa uma classe generica para o repositorio

---
+++ myfinance-web-dotnet-infra/Interfaces/Base/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using myfinance_web_dotnet_domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfinance_web_dotnet_infra.Interfaces.Base
+{
+    internal interface IRepository<T> where T : class
+    {
+        void Cadastrar(T entidade);
+
+        void Excluir(int id);
+
+        List<T> ListarRegistros();
+
+        T RetornarRegistro(int id);
+    }
+}
---
+++ myfinance-web-dotnet-infra/Repository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using myfinance_web_dotnet_infra.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfinance_web_dotnet_infra
+{
+    public abstract class Repository<T> : MyFinanceDbContext, IRepository<T> where T : class
+    {
+        protected Repository(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public void Cadastrar(T entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> ListarRegistros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RetornarRegistro(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
